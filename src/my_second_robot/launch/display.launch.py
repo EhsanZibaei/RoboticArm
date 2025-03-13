2928b90a9811f08bf182ab970d420069ae7105ef
Ehsan Zibaei <ehsan.zibaei@gmail.com>
2025-03-13 22:58:26 +0100

    init

---
+++ src/my_second_robot/launch/display.launch.py
@@ -0,0 +1,23 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    pkg_share = get_package_share_directory('my_second_robot')
+    urdf_path = os.path.join(pkg_share, 'urdf', 'robot_w_screwdriver.urdf')
+
+    with open(urdf_path, 'r') as file:
+        robot_description = file.read()
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description}]
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node
+    ])
