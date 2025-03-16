480912b3cde0e23319cf225d2d5acaa39b51b9d0
Ehsan Zibaei <ehsan.zibaei@gmail.com>
2025-03-16 15:52:01 +0100

    fix controller name

--- src/my_second_robot/launch/use_control_fw.launch.py
+++ src/my_second_robot/launch/use_control_fw.launch.py
@@ -1,12 +1,12 @@
 import os
-from launch import LaunchDescription, LaunchContext
+from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import ExecuteProcess
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessExit
+
+
 
 
 def generate_launch_description():
@@ -22,16 +22,6 @@ def generate_launch_description():
         name='robot_state_publisher',
         parameters=[{'robot_description': robot_description}]
     )
-
-    # joint_state_publisher_node = Node(
-    #     package='joint_state_publisher',
-    #     executable='joint_state_publisher',
-    #     name='joint_state_publisher',
-    #     output='screen',
-    #     parameters=[{'use_gui': True}]
-    # )
-
-
 
     # Controller configuration file
     robot_controllers = PathJoinSubstitution(
@@ -57,7 +47,7 @@ def generate_launch_description():
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_1_controller"],
+        arguments=["joint_controller"],
         output="screen",
     )
 
@@ -69,15 +59,14 @@ def generate_launch_description():
     #     )
     # )
 
-    # rviz_node = Node(
-    #     package="rviz2",
-    #     executable="rviz2",
-    #     name="rviz2",
-    #     output="log",
-    #     arguments=["-d", PathJoinSubstitution(
-    #     [FindPackageShare("my_second_robot"), "config", "rviz_config.rviz"]],
-    #     condition=IfCondition(gui),
-    # )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", PathJoinSubstitution(
+        [FindPackageShare("my_second_robot"), "config", "rviz_config.rviz"])],
+    )
 
     # Foxglove Bridge Node
     foxglove_bridge = Node(
@@ -102,6 +91,7 @@ def generate_launch_description():
         # delay_robot_controller_after_joint_state_broadcaster,
         foxglove_bridge,
         foxglove_studio,
+        rviz_node,
     ])
 
 
