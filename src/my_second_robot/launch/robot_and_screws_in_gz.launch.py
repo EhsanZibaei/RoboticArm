aa0944693d616832e801fdf2407b4b97c8a5c33d
Ehsan Zibaei <ehsan.zibaei@gmail.com>
2025-03-21 18:28:47 +0100

    publish gazebo time

--- src/my_second_robot/launch/robot_and_screws_in_gz.launch.py
+++ src/my_second_robot/launch/robot_and_screws_in_gz.launch.py
@@ -4,6 +4,8 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 import xacro
 
 def generate_launch_description():
@@ -16,6 +18,14 @@ def generate_launch_description():
     robot_desc_path = os.path.join(pkg_share, 'urdf', 'assembly.xacro')
     robot_desc = xacro.process_file(robot_desc_path).toxml()  # Convert xacro to URDF XML
 
+    # Robot State Publisher (publishes the processed URDF)
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_desc}]
+    )
+
     # Gazebo launch with Bullet physics engine (optional)
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
@@ -24,14 +34,6 @@ def generate_launch_description():
         launch_arguments={
             'gz_args': f'{world_file} -r --physics-engine gz-physics-bullet-featherstone-plugin'
         }.items()
-    )
-
-    # Robot State Publisher (publishes the processed URDF)
-    robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[{'robot_description': robot_desc}]
     )
 
     # Spawn robot in Gazebo using the processed URDF
@@ -46,8 +48,72 @@ def generate_launch_description():
         output='screen'
     )
 
+    # Controller configuration file
+    robot_controllers = PathJoinSubstitution(
+        [FindPackageShare("my_second_robot"), "config", "ros2_control_config.yaml"]
+    )
+
+        # Start ros2_control
+    ros2_control_node = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[robot_controllers],
+        output='screen'
+    )
+    
+    # Spawning controllers
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+        output="screen",
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_controller"],
+        output="screen",
+    )
+
+
+    clock_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
+        output='screen',
+    )
+
+
+    # Foxglove Bridge Node
+    foxglove_bridge = Node(
+        package="foxglove_bridge",
+        executable="foxglove_bridge",
+        name="foxglove_bridge",
+        output="log",
+        parameters=[{"port": 8765}],  # Default WebSocket port
+    )
+
+    # Command to launch Foxglove Studio
+    foxglove_studio = ExecuteProcess(
+        cmd=["/opt/Foxglove Studio/foxglove-studio"],
+    )
+
+
+
     return LaunchDescription([
         robot_state_publisher,
-        spawn_robot,
+        
+        ros2_control_node,
+        joint_state_broadcaster_spawner,
+        robot_controller_spawner,
+        
+        
         gazebo,
+        spawn_robot,
+
+
+        clock_bridge,
+        foxglove_bridge,
+        foxglove_studio,
     ])
