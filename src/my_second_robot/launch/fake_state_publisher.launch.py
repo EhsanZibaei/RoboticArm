73b9e2dd4300bc18b0fb14cdf1b304edf2fe3d9a
Ehsan Zibaei <ehsan.zibaei@gmail.com>
2025-03-25 11:01:25 +0100

    fix joint_state_publisher launch file

--- src/my_second_robot/launch/fake_state_publisher.launch.py
+++ src/my_second_robot/launch/fake_state_publisher.launch.py
@@ -2,14 +2,15 @@ import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from xacro import process_file
 
 def generate_launch_description():
     pkg_share = get_package_share_directory('my_second_robot')
-    urdf_path = os.path.join(pkg_share, 'urdf', 'robot_w_screwdriver.urdf')
+    urdf_path = os.path.join(pkg_share, 'urdf', 'assembly.xacro')
     rviz_config_path = os.path.join(pkg_share, 'config', 'rviz_config.rviz')  # Path to RViz config
 
-    with open(urdf_path, 'r') as file:
-        robot_description = file.read()
+    # Parse the xacro file
+    robot_description = process_file(urdf_path).toxml()
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
