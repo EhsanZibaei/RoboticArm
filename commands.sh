c7cbc3e288540c1589e92f7a71dd2fbe32e07918
Ehsan Zibaei <ehsan.zibaei@gmail.com>
2025-03-25 17:16:01 +0100

    use cart_position example to load gz_ros2 package

--- commands.sh
+++ commands.sh
@@ -1,6 +1,6 @@
 rm -rf build/ install/ log/
-colcon build 
+colcon build --packages-select my_second_robot
 source install/setup.bash
 # ros2 launch my_second_robot fake_state_publisher.launch.py
-# ros2 launch my_second_robot robot_and_screws_in_gz.launch.py
-ros2 launch moving3 demo.launch.py
+ros2 launch my_second_robot start_gz_ros2_control_from_template.launch.py
+# ros2 launch moving3 demo.launch.py
