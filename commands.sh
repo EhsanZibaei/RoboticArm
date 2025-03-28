adf366366493fb16c441e581661944eb21aa6c27
Ehsan Zibaei <ehsan.zibaei@gmail.com>
2025-03-28 14:52:28 +0100

    launch only moveit based on independent xacros

--- commands.sh
+++ commands.sh
@@ -1,6 +1,5 @@
 rm -rf build/ install/ log/
-colcon build --packages-select my_second_robot moving5
-source install/setup.bash
+colcon build --packages-select moving6
 # ros2 launch my_second_robot start_gz_ros2_control_from_template.launch.py
-ros2 launch my_second_robot add_moveit.launch.py
+ros2 launch moving6 only_moveit.launch.py
 # ros2 launch moving5 demo.launch.py
---
+++ src/moving6/launch/only_moveit.launch.py
@@ -0,0 +1,61 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+from moveit_configs_utils import MoveItConfigsBuilder
+from ament_index_python.packages import get_package_share_directory
+import xacro
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    # Define xacro mappings for the robot description file
+    launch_arguments = {
+        "robot_ip": "xxx.yyy.zzz.www",
+        "use_fake_hardware": "true",
+        "gripper": "robotiq_2f_85",
+        "dof": "7",
+    }
+
+    # Load the robot configuration
+    moveit_config = (
+        MoveItConfigsBuilder(
+            "gen3", package_name="moving6"
+        )
+        .robot_description(mappings=launch_arguments)
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
+        .planning_scene_monitor(
+            publish_robot_description=True, publish_robot_description_semantic=True
+        )
+        .planning_pipelines(
+            pipelines=["ompl", "stomp", "pilz_industrial_motion_planner"]
+        )
+        .to_moveit_configs()
+    )
+
+    pkg_my_robot = get_package_share_directory('moving6')
+    # robot_description_content = xacro.process_file(os.path.join(pkg_my_robot, 'config', 'circu.urdf.xacro')).toxml()
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': moveit_config.robot_description}]
+    )
+    # Start the actual move_group node/action server
+    run_move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[moveit_config.to_dict()],
+    )
+
+    return LaunchDescription(
+    [
+        robot_state_publisher_node,
+        run_move_group_node,
+    ]
+    )
