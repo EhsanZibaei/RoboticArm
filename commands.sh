08a8cb1d254ba1a60ceb4894212807b32dd1b1b2
Ehsan Zibaei <ehsan.zibaei@gmail.com>
2025-03-29 16:23:55 +0100

    run moveit alongside ros2_control and gz

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 rm -rf build/ install/ log/
 colcon build --packages-select moving6
 # ros2 launch my_second_robot start_gz_ros2_control_from_template.launch.py
-ros2 launch moving6 gz_ros2_demo.launch.py
+ros2 launch moving6 moveit_gz_ros2_control.launch.py
 # ros2 launch moving5 demo.launch.py
