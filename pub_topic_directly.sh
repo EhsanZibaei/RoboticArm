88a9089a292a8b1c7db556c3ca319eb3ef5dd619
Ehsan Zibaei <ehsan.zibaei@gmail.com>
2025-03-31 19:49:38 +0200

    add ros2_control topic publish file

---
+++ pub_topic_directly.sh
@@ -0,0 +1,11 @@
+ros2 topic pub /g1_controller/joint_trajectory trajectory_msgs/msg/JointTrajectory "{
+  header: {
+    stamp: {sec: 0, nanosec: 0},
+    frame_id: ''
+  },
+  joint_names: ['joint_1', 'joint_2', 'joint_3', 'joint_4', 'joint_5', 'joint_6'],
+  points: [{
+    positions: [0.0, 0.5, 0.0, 0.0, 0.0, 0.0],
+    time_from_start: {sec: 3, nanosec: 0}
+  }]
+}"
